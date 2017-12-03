67e075580e4c2358a29fd448f9e8ae002a0a42af
Mark Long <mhplong@windstream.net>
2017-12-02 19:48:06 -0600

    Day 2 Part 2

--- Advent2017/Day2/Day2.swift
+++ Advent2017/Day2/Day2.swift
@@ -13,18 +13,23 @@ input = input.trimmingCharacters(in: ["\r", "\n"])
 var checksum: [Int] = []
 var rows = input.split(separator: "\n")
 for row in rows {
-    var lowestNumber: Int!
-    var highestNumber = 0
-    for number in row.split(separator: "\t") {
-        let value = Int(number)!
-        
-        lowestNumber = lowestNumber ?? value
-        
-        lowestNumber = min(lowestNumber, value)
-        highestNumber = max(highestNumber, value)
+    var lowerNumber = 1
+    var higherNumber = 2
+    var numbers = row.split(separator: "\t")
+    for baseIndex in 0..<numbers.count {
+        let value = Int(numbers[baseIndex])!
+        for index in baseIndex..<numbers.count {
+            let secondValue = Int(numbers[(index+1) % numbers.count])!
+            let lowerBound = min(value, secondValue)
+            let upperBound = max(value, secondValue)
+            if (upperBound % lowerBound == 0) {
+                lowerNumber = lowerBound
+                higherNumber = upperBound
+            }
+        }
     }
-    print("\(lowestNumber!): \(highestNumber)")
-    checksum.append(highestNumber - lowestNumber!)
+    print("\(higherNumber): \(lowerNumber)")
+    checksum.append(higherNumber / lowerNumber)
 }
 print(checksum)
 print(checksum.reduce(0, +))
